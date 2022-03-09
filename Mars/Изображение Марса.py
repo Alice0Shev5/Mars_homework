f44dd6ab510534ba69dc9ea5bf79ab1151fa4d52
Alice0Shev5 <94836884+Alice0Shev5@users.noreply.github.com>
2022-03-09 21:53:54 +0400

    Add files via upload

---
+++ Mars/Изображение Марса.py
@@ -0,0 +1,32 @@
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def run():
+    return "Миссия Колонизация Марса"
+
+
+@app.route('/index')
+def index():
+    return "И на Марсе будут яблони цвести!"
+
+
+@app.route('/promotion')
+def advertising():
+    text = ['Человечество вырастает из детства.',
+            'Человечеству мала одна планета.',
+            'Мы сделаем обитаемыми безжизненные пока планеты.',
+            'И начнем с Марса!',
+            'Присоединяйся!']
+    return '</br>'.join(text)
+
+
+@app.route('/image_sample')
+def image():
+    return render_template('Изображение Марса.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')

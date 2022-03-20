0df1cd4899b14f82ac7d6f1df2faed462ba960b1
Alice0Shev5 <94836884+Alice0Shev5@users.noreply.github.com>
2022-03-20 22:10:35 +0400

    Add files via upload

---
+++ Mars/Тренировки в полете.py
@@ -0,0 +1,27 @@
+import json
+
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index/<string:title>')
+def index(title='Готовимся к миссии'):
+    param = {}
+    param['title'] = title
+    return render_template('base.html', **param)
+
+
+@app.route('/training/<string:prof>')
+def training(prof):
+    if prof in ['инженер', 'строитель']:
+        print(True)
+
+    return render_template('training.html', prof=prof)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
+
+
